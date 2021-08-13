664d136868834b844b2ce0e1abeb57f11871b87d
Semih <31005189+sualtindakiadam@users.noreply.github.com>
2021-08-13 15:55:51 +0300

    seçili noktayı veri tabanına kaydetme yapıldı

--- TravelBook/ViewController.swift
+++ TravelBook/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import MapKit //haritaları kullanabilmek için
 import  CoreLocation // konum işlemleri için
+import CoreData
 
 class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDelegate { // delegate edebilmek için ekle
     
@@ -16,7 +17,8 @@ class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDeleg
     @IBOutlet weak var mapView: MKMapView!
     
     
-    
+    var choosenLat = Double()
+    var choosenLong = Double()
     var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
@@ -29,6 +31,8 @@ class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDeleg
         locationManager.startUpdatingLocation() // kullanıcının yerini almaya başlıyoruz
         
         
+ 
+        
         let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(chooseLocation(gestureRecognizer:))) // basılı tutma işlemi için tanımlama
         
         gestureRecognizer.minimumPressDuration = 2 // basılı tutma süresini ayarlar
@@ -40,6 +44,9 @@ class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDeleg
         if gestureRecognizer.state == .began{ //dokunma başladı mı
             let touchPoint = gestureRecognizer.location(in: self.mapView) // mapview üzerinde dokunulan noktayı al
             let touchedCordinates = self.mapView.convert(touchPoint, toCoordinateFrom: self.mapView) // çevir
+            
+            choosenLat = touchedCordinates.latitude
+            choosenLong = touchedCordinates.longitude
             
             let annotation = MKPointAnnotation()
             annotation.coordinate = touchedCordinates
@@ -59,5 +66,30 @@ class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDeleg
         let region = MKCoordinateRegion(center: location, span: span) // konum ve zoom ayarlarını birleştir
         mapView.setRegion(region, animated: true) // mapta göster
     }
+    
+    @IBAction func saveBtnClicked(_ sender: Any) {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newPlace = NSEntityDescription.insertNewObject(forEntityName: "Places", into: context)
+        newPlace.setValue(nameTextView.text, forKey: "title")
+        newPlace.setValue(commentTextView.text, forKey: "subTitle")
+        newPlace.setValue(choosenLat, forKey: "latitude")
+        newPlace.setValue(choosenLong, forKey: "longitude")
+        newPlace.setValue(UUID(), forKey: "id")
+        
+        do{
+            
+            try  context.save()
+            print("saved")
+            
+        }catch{
+            print("error saved")
+            
+        }
+       
+    }
+    
 }
 
